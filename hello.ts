e59924b2c3ebb4420c048b3119be894fd6386610
Ganesh <lmuraliganesh@gmail.com>
2026-08-15 09:26:19 +0530

    day3

---
+++ hello.ts
@@ -0,0 +1,26 @@
+function welcome(name:string): string{
+    return `Hello, ${name}`!;
+}
+const message : string = welcome("Alex");
+console.log(message);
+
+let babyboy: boolean = true;
+let babygirl = false;
+
+console.log(babyboy);
+console.log(babygirl);
+
+let decimal : number = 89;
+let ganesh : string = "Ganesh"
+let flot : number = 3.22;
+let age : number = 40;
+let sentence : string = `Hi i am ${ganesh} will be turning ${age+ 1} next year`;
+
+console.log(ganesh);
+console.log(decimal);
+console.log(flot);
+console.log(sentence);
+
+
+let built = 190;
+console.log(built);
